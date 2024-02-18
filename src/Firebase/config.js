d207316ff90a4a206cc18e6d795adb9c1c266553
Oybek <saminovo150947@gmail.com>
2024-02-18 17:23:45 +0500

    aaaddddd

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore"
 import {getStorage} from "firebase/storage"
 
 export const firebaseConfig = {
-
- apiKey: "AIzaSyDSRiIMdDTHeaGMZyY068dva000-1AcHuU",
-  authDomain: "shop-95504.firebaseapp.com",
-  projectId: "shop-95504",
-  storageBucket: "shop-95504.appspot.com",
-  messagingSenderId: "1026779474318",
-  appId: "1:1026779474318:web:93d2f2a320a55e83531cdf"
+ apiKey: "AIzaSyDTIhnGvV3nvIDFTk9Bor0t6j66a0kE7o8",
+  authDomain: "shopping-4a8d6.firebaseapp.com",
+  databaseURL: "https://shopping-4a8d6-default-rtdb.firebaseio.com",
+  projectId: "shopping-4a8d6",
+  storageBucket: "shopping-4a8d6.appspot.com",
+  messagingSenderId: "493510002186",
+  appId: "1:493510002186:web:d52bfd301625666d5b81f3"
 // ~~~~~~~~ Team accauntniki~~~~~~~~~~~~//
 // apiKey: "AIzaSyBIr-FA-_IQFBdJpAlJyr3Qtox2Id8MJRQ",
 //   authDomain: "shopping-791be.firebaseapp.com",
